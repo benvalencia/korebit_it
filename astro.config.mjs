0b3d42c8980a000163f71a934fd5ca8baf5188f3
Ben <benyamin.ak@hotmail.com>
2026-01-26 17:38:38 +0100

    feat(#3): estilos del header

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 const site = 'https://benvalencia.github.io/korebit_it'
+const base = '/korebit_it'
+
 export default defineConfig({
   site,
+  base,
   vite: {
     plugins: [tailwindcss()],
   },
